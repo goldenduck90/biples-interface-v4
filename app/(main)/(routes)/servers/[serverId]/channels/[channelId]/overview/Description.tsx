1061ea751026525ad744fa6831decb4f8137e943
goldenduck90 <goldenduck90919@gmail.com>
2024-02-14 15:11:45 +0900

    fix: build errors

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/overview/Description.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/overview/Description.tsx
@@ -6,7 +6,7 @@ export default function Description() {
       </div>
       <div className="w-full font-TT Firs Neue text-white text-[15px] ">
         Lorem lpsum is simply dummy text of printin and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the
+        Lorem Ipsum has been the industry&apos;s standard dummy text ever since the
         1500s, when an unknown printer took a galley of type and scrambled it to
         make a type specimen book. It has survived not only five centuries, but
         also the leap into electronic typesetting, remaining essentially
--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/overview/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/overview/page.tsx
@@ -62,7 +62,7 @@ const MyNFTsPage: React.FC = () => {
             description={
               <span>
                 Lorem lpsum is simply dummy text of printin and typesetting
-                industry. <b>Lorem</b> Ipsum has been the industry's standard
+                industry. <b>Lorem</b> Ipsum has been the industry&apos;s standard
                 dummy text ever since the 1500s, when an unknown printer took a
                 galley of type and scrambled it to make a type specimen book. It
                 has survived not only five centuries, but also the leap into
