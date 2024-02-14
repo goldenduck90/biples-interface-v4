178c5eb030c2b1978fe8ac0c1cd5c86fa20e1f48
goldenduck90 <goldenduck90919@gmail.com>
2024-02-14 11:33:54 +0900

    feat: update project config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil"
-    });
-
-    return config;
-  },
   images: {
-    domains: [
-      "uploadthing.com",
-      "utfs.io"
-    ]
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
